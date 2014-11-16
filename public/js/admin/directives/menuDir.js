1c8a68251b4a3fa7df618f1f5c6cb0747dd3c35b
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2014-11-16 23:51:12 +0200

    added some icons

--- public/js/admin/directives/menuDir.js
+++ public/js/admin/directives/menuDir.js
@@ -2,28 +2,33 @@ angular.module('Teach4Tech.Admin.Directives.Menu', [])
 .directive('menu', [function(){
  return {
       restrict: 'A',
-      template: '<li ng-repeat="item in menuItems" ng-class="{active: isActive(item.root)}"><a ui-sref="{{item.state}}">{{item.title}}</a></li>',
+      template: '<li ng-repeat="item in menuItems" ng-class="{active: isActive(item.root)}"><a ui-sref="{{item.state}}"><span class="{{item.iconClass}}"></span>&nbsp;&nbsp;{{item.title}}</a></li>',
       controller: ['$scope', '$state', function ($scope, $state) {
       	$scope.menuItems = [{
-      	  title: 'Статистика',
-  		  state: 'statistics',
-  		  root: 'statistics'
-      	}, {
-  		  title: 'Видеозаписи',
-		  state: 'videos.list',
-		  root: 'videos'
-      	}, {
-  		  title: 'Статьи',
-		  state: 'articles',
-		  root: 'articles'
-      	}, {
-  		  title: 'Курсы',
-		  state: 'courses',
-		  root: 'courses'
-      	}, {
-  		  title: 'Сообщения',
-		  state: 'messages',
-		  root: 'messages'
+      	    title: 'Статистика',
+  		      state: 'statistics',
+  		      root: 'statistics',
+            iconClass: 'glyphicon glyphicon-stats'
+      	  }, {
+  		      title: 'Видеозаписи',
+		        state: 'videos.list',
+		        root: 'videos',
+            iconClass: 'glyphicon glyphicon-film'
+      	  }, {
+  		      title: 'Статьи',
+		        state: 'articles',
+		        root: 'articles',
+            iconClass: 'glyphicon glyphicon-pencil'
+      	  }, {
+  		      title: 'Курсы',
+		        state: 'courses',
+		        root: 'courses',
+            iconClass: 'glyphicon glyphicon-list-alt'
+      	  }, {
+  		      title: 'Сообщения',
+		        state: 'messages',
+		        root: 'messages',
+            iconClass: 'glyphicon glyphicon-comment'
       	}];
 
       	$scope.isActive = function(root) {
