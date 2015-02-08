b92a16634c9a95d4434e0e0a8a2e53da73ff2d9e
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2015-02-08 17:06:59 +0200

    fixed logo image, and users online counter

--- helpers/index.js
+++ helpers/index.js
@@ -2,14 +2,24 @@ var winston = require('winston');
 
 module.exports = function(app){
 
-  app.use(function(req, res, next){
-  	
-  	if (!!req.user) {
-  	  res.locals.isLoggedIn = !req.user.anonymous;
+  app.use(_setUser);
+  app.use(_setUsersOnline);
+
+  function _setUser(req, res, next) {
+    if (!!req.user) {
+      res.locals.isLoggedIn = !req.user.anonymous;
       res.locals.userName = req.user.name;
-  	}
-    
+    }
+
     next();
-  });
-  
+  };
+
+  function _setUsersOnline(req, res, next) {
+    if (!!req.online) {
+      res.locals.online = req.online.length;
+    }
+
+    next();
+  };
+
 };
